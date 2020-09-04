921bb80222465bc781807d797dbea6130ee0fbbb
mick mcmahon <mick.mcmahon@gmail.com>
2020-09-04 18:01:14 +0100

    more

--- src/App.js
+++ src/App.js
@@ -11,7 +11,102 @@ export default function App() {
       <div className={css.item} tabIndex="0">
         <span>Here</span>
         <div className={css.yore}>
-          <ul>div.ul-line*42</ul>
+          <div class={css.cell} style={{ "--cell": 1 }}>
+            01
+          </div>
+          <div class={css.cell} style={{ "--cell": 2 }}>
+            02
+          </div>
+          <div class={css.cell} style={{ "--cell": 3 }}>
+            03
+          </div>
+          <div class={css.cell} style={{ "--cell": 4 }}>
+            04
+          </div>
+          <div class={css.cell} style={{ "--cell": 5 }}>
+            05
+          </div>
+          <div class={css.cell} style={{ "--cell": 6 }}>
+            06
+          </div>
+          <div class={css.cell} style={{ "--cell": 7 }}>
+            07
+          </div>
+          <div class={css.cell} style={{ "--cell": 8 }}>
+            08
+          </div>
+          <div class={css.cell} style={{ "--cell": 9 }}>
+            09
+          </div>
+          <div class={css.cell} style={{ "--cell": 10 }}>
+            10
+          </div>
+          <div class={css.cell} style={{ "--cell": 11 }}>
+            11
+          </div>
+          <div class={css.cell} style={{ "--cell": 12 }}>
+            12
+          </div>
+          <div class={css.cell} style={{ "--cell": 13 }}>
+            13
+          </div>
+          <div class={css.cell} style={{ "--cell": 14 }}>
+            14
+          </div>
+          <div class={css.cell} style={{ "--cell": 15 }}>
+            15
+          </div>
+          <div class={css.cell} style={{ "--cell": 16 }}>
+            16
+          </div>
+          <div class={css.cell} style={{ "--cell": 17 }}>
+            17
+          </div>
+          <div class={css.cell} style={{ "--cell": 18 }}>
+            18
+          </div>
+          <div class={css.cell} style={{ "--cell": 19 }}>
+            19
+          </div>
+          <div class={css.cell} style={{ "--cell": 20 }}>
+            20
+          </div>
+          <div class={css.cell} style={{ "--cell": 21 }}>
+            21
+          </div>
+          <div class={css.cell} style={{ "--cell": 22 }}>
+            22
+          </div>
+          <div class={css.cell} style={{ "--cell": 23 }}>
+            23
+          </div>
+          <div class={css.cell} style={{ "--cell": 24 }}>
+            24
+          </div>
+          <div class={css.cell} style={{ "--cell": 25 }}>
+            25
+          </div>
+          <div class={css.cell} style={{ "--cell": 26 }}>
+            26
+          </div>
+          <div class={css.cell} style={{ "--cell": 27 }}>
+            27
+          </div>
+          <div class={css.cell} style={{ "--cell": 28 }}>
+            28
+          </div>
+          <div class={css.cell} style={{ "--cell": 29 }}>
+            29
+          </div>
+          <div class={css.cell} style={{ "--cell": 30 }}>
+            30
+          </div>
+          <div class={css.cell} style={{ "--cell": 31 }}>
+            31
+          </div>
+          <div class={css.cell} style={{ "--cell": 32 }}>
+            32
+          </div>
         </div>
       </div>
     </div>
